703f6c922637f9402d300b015bbab0d1289ebb03
Bulzaj <ernestsado94@gmail.com>
2022-05-19 16:15:03 +0200

    refactor - reset css, add roboto font

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
+import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/home-page/HomePage";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <CssBaseline />
       <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </>
   );
 }
 
